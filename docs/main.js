310e3519e116c2b308ffb77d230b77b7416241a9
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-07-29 23:52:03 -0400

    Got dark mode working 📦

--- docs/main.js
+++ docs/main.js
@@ -3,8 +3,17 @@ let lightModeButton = document.getElementsByClassName("morning-mode-button")[0];
 let body = document.getElementsByTagName("body")[0];
 let links = document.getElementsByTagName("a");
 
+
+body.onload = function (){
+    lightModeButton.style.visibility = "hidden";
+}
+
+
 darkModeButton.addEventListener("click", function (){
-    darkModeButton.style.display = "none";
+    darkModeButton.style.visibility = "hidden";
+    lightModeButton.style.visibility = "visible";
+
+
     body.style.backgroundColor = "black";
     body.style.color = "white";
     links[0].style.color = "white";
@@ -12,8 +21,18 @@ darkModeButton.addEventListener("click", function (){
     links[2].style.color = "white";
     links[3].style.color = "white";
     links[4].style.color = "white";
-    lightModeButton.style.visibility = "visible";
 });
 
-//lightModeButton.style.visibility = "visible";
-    
+lightModeButton.addEventListener("click", function (){
+    lightModeButton.style.visibility = "hidden";
+    darkModeButton.style.visibility = "visible";
+
+
+    body.style.backgroundColor = "#f5f5f5";
+    body.style.color = "black";
+    links[0].style.color = "black";
+    links[1].style.color = "black";
+    links[2].style.color = "black";
+    links[3].style.color = "black";
+    links[4].style.color = "black";
+});
